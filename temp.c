3b6d0ee45b5969ed1877595cc4c91ed034140790
tanermetin <stanermetin@gmail.com>
2017-07-07 15:55:49 +0200

    cmt

--- temp.c
+++ temp.c
@@ -1,10 +1,10 @@
-#include<stdio.h>
+1#include<stdio.h>
 
 int main()
 {
   int a=5;
   float b=5.1;
-  char c="char";
+  char c='c';
 printf("temp file");
  printf("a:%d\n b:%f\n c:%c", a,b,c);
 return 0;
