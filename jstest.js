792cf936ab1e96c919d9dda4ff75fe94ff32786d
tanermetin <stanermetin@gmail.com>
2017-07-08 15:08:45 +0200

    jstest.js added

---
+++ jstest.js
@@ -0,0 +1,14 @@
+<body>
+
+<h2>first js</h2>
+
+<button type="button">
+
+onclick=document.getElementById('demo').innerHTML=Date()">
+
+Date and Time.</button>
+
+<p id="demo"></p>
+
+</body>
+</html>
