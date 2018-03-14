0547ece0958559b40c254be81d6b8cfd0c36d245
tanermetin <stanermetin@gmail.com>
2018-03-14 02:11:38 +0100

    14.03

--- sh/hrank.sh
+++ sh/hrank.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+
 echo 'ahoi'
 
 for var in {0..100}
@@ -14,3 +15,20 @@ echo 'Como se dice?'
 read var
 
 echo "se dice" $var
+
+echo 'enter first value:'
+read var1
+
+echo 'enter second value:'
+read var2
+
+
+sum=$(( var1+var2 ))
+sub=$(( var1-var2 ))
+mult=$(( var1*var2 ))
+div=$(( var1/var2 ))
+
+echo "$sum"
+echo "$sub"
+echo "$mult"
+echo "$div"
