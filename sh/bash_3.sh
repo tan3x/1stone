edb4d27752baba29293745a7429414f8f23441df
tanermetin <stanermetin@gmail.com>
2018-03-21 15:59:14 +0100

    head/cut

--- sh/bash_3.sh
+++ sh/bash_3.sh
@@ -1,40 +1,87 @@
 #!/bin/bash
 
 
-echo 'Enter 2 integers'
-read var1
-read var2
+# echo 'Enter 2 integers'
+# read var1
+# read var2
 
-if [ $var1 -gt $var2 ]; then
-	echo "X is greater than Y"
-elif [[ $var1 -eq $var2 ]]; then
-	echo 'X is equals to Y'
-elif [[ $var1 -lt $var2  ]]; then
-	echo 'X is less than Y'
-fi
+# if [ $var1 -gt $var2 ]; then
+# 	echo "X is greater than Y"
+# elif [[ $var1 -eq $var2 ]]; then
+# 	echo 'X is equals to Y'
+# elif [[ $var1 -lt $var2  ]]; then
+# 	echo 'X is less than Y'
+# fi
 
-echo 'Y/N'
-read var3
-if [ $var3 == 'y' -o $var3 == 'Y' ]; then
-	echo 'YES'
-elif [ $var3 == 'n' -o $var3 == 'N' ]; then
-	echo 'NO'
-fi
+# echo 'Y/N'
+# read var3
+# if [ $var3 == 'y' -o $var3 == 'Y' ]; then
+# 	echo 'YES'
+# elif [ $var3 == 'n' -o $var3 == 'N' ]; then
+# 	echo 'NO'
+# fi
 
-echo 'Bermuda Triangle'
-read edge1
-read edge2
-read edge3
+# echo 'Bermuda Triangle'
 
-if [[ $edge1 == edge2 && $edge2 == $edge3 ]]
-	then
-	echo "EQUILATERAL"
-elif [[ $edge1 == $edge2 || $edge2 == $edge3 || $edge1 == $edge3 ]]
-	then 
-	echo "ISOSCELES"
-else
-	echo "SCALENE"
-fi
+# read edge1
+# read edge2
+# read edge3
+
+# if [[ $edge1 == $edge2 && $edge2 == $edge3 ]]
+# 	then
+# 	echo "EQUILATERAL"
+# elif [[ $edge1 == $edge2 || $edge2 == $edge3 || $edge1 == $edge3 ]]
+# 	then 
+# 	echo "ISOSCELES"
+# else
+# 	echo "SCALENE"
+# fi
+
+# read calc
+# echo $calc | bc -l | xargs printf '%.3f'
+
+# echo 'number of integers:'
+# read nri
+# cnt=0
+# total=0
+# while [ $nri -lt 500 ]
+# do
+# 	while [ $cnt -lt $nri ]
+# 	do
+# 		echo 'enter integers'
+# 		read -r val
+# 		total=$((total+val))
+# 		((cnt++)) 
+# 	done
+# 	printf "%.3f" $(echo $total/$nri | bc -l) 
+# 	break 
+# done
+
+ 
+while read line 
+do
+	echo $line | cut -c3 - #just third letter
+	echo $line | cut -c2,7 - #second and seventh letter
+	echo $line | cut -c1-4 - #first four letters
+	echo $line | cut -c2-7 - #between second and seventh
+	echo $line | cut -c2-7 - #between second and seventh
+	echo $line | cut -c13- #until end
+	echo $line | cut -d' ' -f4
+	echo $line | cut -d' ' -f1-3 #first 3 words
+	echo $line | cut -f2- #from second until end / TAB is default delimiter.
+
+	awk -F "   " '{print $1 $2 $3}' city
+	#head -n 20 city
+	head -c20 city
+	head -22 | tail -11 city
+	break 
+done 
+
+
+
+
+
+
 
 
 
