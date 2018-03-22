05a415827fd3bd5bf109be9fd65f20a53da927ee
tanermetin <stanermetin@gmail.com>
2018-03-22 20:31:22 +0100

    uniq

--- sh/bash_3.sh
+++ sh/bash_3.sh
@@ -58,29 +58,48 @@
 # done
 
  
-while read line 
-do
-	echo $line | cut -c3 - #just third letter
-	echo $line | cut -c2,7 - #second and seventh letter
-	echo $line | cut -c1-4 - #first four letters
-	echo $line | cut -c2-7 - #between second and seventh
-	echo $line | cut -c2-7 - #between second and seventh
-	echo $line | cut -c13- #until end
-	echo $line | cut -d' ' -f4
-	echo $line | cut -d' ' -f1-3 #first 3 words
-	echo $line | cut -f2- #from second until end / TAB is default delimiter.
+# while read line 
+# do
+# 	echo $line | cut -c3 - #just third letter
+# 	echo $line | cut -c2,7 - #second and seventh letter
+# 	echo $line | cut -c1-4 - #first four letters
+# 	echo $line | cut -c2-7 - #between second and seventh
+# 	echo $line | cut -c2-7 - #between second and seventh
+# 	echo $line | cut -c13- #until end
+# 	echo $line | cut -d' ' -f4
+# 	echo $line | cut -d' ' -f1-3 #first 3 words
+# 	echo $line | cut -f2- #from second until end / TAB is default delimiter.
 
-	awk -F "   " '{print $1 $2 $3}' city
-	#head -n 20 city
-	head -c20 city
-	head -22 | tail -11 city
-	break 
-done 
+# 	awk -F "   " '{print $1 $2 $3}' city
+# 	#head -n 20 city
+# 	head -c20 city
+# 	head -22 | tail -11 city
+# 	break 
+# done
+
+#TR
+# tr 'S' 'J' < food
+# tr -d [:lower:] < food
+# tr -d [:upper:] < food
+# tr -s ' ' < food
+
+#SORT 
+# cat food | sort -k1 #lexicographical order 
+# cat food | sort -r -k1 #reverse lexicographical order 
+# cat numbers | sort -g #floating sort
+# cat numbers | sort -r -g #floating sort
+
+# sort -t $'\t' -k2,2 -n -r temp # TAB seperated
+# sort -t $'|' -k2,2 -n -r  temp # | seperated 
+
+# uniq -d dupl
+# uniq -c dupl
+# uniq -ci | cut -c7- dupl # case sensitive and eliminates first gap
+# uniq -u dupl  #display lines that are not repeated 
 
 
-
-
-
+paste -s food 
+paste -s -d '\t\t\n' food 
 
 
 
