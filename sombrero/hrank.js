5a6356d516c70b1593cc40e0af466ef94ec66fc8
tanermetin <stanermetin@gmail.com>
2018-03-27 23:56:11 +0200

    cogito ergo sum

--- sombrero/hrank.js
+++ sombrero/hrank.js
@@ -35,7 +35,7 @@ function factorial(input){
         return 1;
     else
         {return input * factorial(input-1)}
-    
+
 }
 function main() {
     // Write your code here. Read input using 'readLine()' and print output using 'console.log()'.
@@ -50,6 +50,36 @@ function main() {
     function perimeter(r){
         return 2*r*PI;
     }
-    console.log(area(r));
-    console.log(perimeter(r))
+    console.log('Factorial: '+factorial(5))
+    console.log('Area:' + area(r));
+    console.log('Perimeter:' + perimeter(r))
+
+    // const score = +(readLine());
+    let score = 23;
+    console.log('Grade:'+getGrade(score));
+}
+
+
+function getGrade(score) {
+    let grade;
+    
+    if ( 0 <= score && score <= 5 ) {
+        grade='F';
+    }
+    else if (5<score && score <=10){
+        grade='E';
+    }
+    else if (10<score && score <=15 ){
+        grade='D';
+    }
+    else if ( 15 < score && score <= 20 ) {
+        grade='C';
+    }
+    else if ( 20 < score && score <= 25 ) {
+        grade='B';
+    }
+    else if ( 25 < score && score <= 30 ) {
+        grade='A';
+    } 
+    return grade;
 }
--- sombrero/sec.js
+++ sombrero/sec.js
@@ -3,8 +3,6 @@ var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('ahoi.\n'); 
-}).listen(3000, '127.0.0.1');
+}).listen(3001, '127.0.0.1');
 
-console.log('Server running at http://127.0.0.1:3000/');
-
-
+console.log('Server running at http://127.0.0.1:3001/');
