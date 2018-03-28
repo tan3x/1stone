05f5f2ae776f6b6009fc867325c8296b5a3399c6
tanermetin <stanermetin@gmail.com>
2018-03-28 18:04:12 +0200

    18

---
+++ python/hrank/d18.py
@@ -0,0 +1,69 @@
+#!/bin/python
+
+import sys
+import random
+import numpy
+
+
+
+
+#day 18 Queues & Stack // Palindrome
+
+s = 'ahoioha' ;
+
+l = len(s)
+
+class Solution:
+
+	def __init__(self):
+		self.stack=[]
+		self.queue=[]
+		self.length=0
+
+	def pushCharacter(self, letter):
+		self.stack.insert(0, letter)
+
+	def enqueueCharacter(self, ch):
+		self.queue.append(ch)
+
+	def popCharacter(self):
+		return self.stack.pop()
+
+	def dequeueCharacter(self):
+		return self.queue.pop()
+
+
+#Create the Solution class object
+obj=Solution()   
+
+l=len(s)
+# push/enqueue all the characters of string s to stack
+for i in range(l):
+    obj.pushCharacter(s[i])
+    obj.enqueueCharacter(s[i])
+    
+isPalindrome=True
+'''
+pop the top character from stack
+dequeue the first character from queue
+compare both the characters
+''' 
+for i in range(l / 2):
+    if obj.popCharacter()!=obj.dequeueCharacter():
+        isPalindrome=False
+        break
+#finally print whether string s is palindrome or not.
+if isPalindrome:
+    sys.stdout.write ("The word, "+s+", is a palindrome.")
+else:
+    sys.stdout.write ("The word, "+s+", is not a palindrome.")    
+    
+        
+
+
+
+
+
+
+
+
