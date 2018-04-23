830b9af5596af9026cb87e4da894964edd55c379
tme <stanermetin@gmail.com>
2018-04-23 15:07:59 +0200

    P4ML

--- c/cs50/pset3/music/helpers.c
+++ c/cs50/pset3/music/helpers.c
@@ -8,26 +8,12 @@
 
 // Converts a fraction formatted as X/Y to eighths
 int duration(string fraction)
-int duration(string fraction)
 {
     if(fraction[0] == '1')
     {
-        if(fraction[2] == '8')
-        {
-            return 1;
-        }
-        else if(fraction[2] == '4')
-        {
-            return 2;
-        }
-        else if(fraction[2] == '2')
-        {
-            return 4;
-        }
-        else if(fraction[2] == '1')
-        {
-            return 8;
-        }
+        float x = 8 / atoi(&fraction[2]);;
+
+        return x;
     }
     else if(fraction[0] == '3' && fraction[2] == '8')
     {
@@ -46,18 +32,18 @@ int frequency(string note)
     int n = 0;
     int octave;
     if (strlen(note)==3){
+
         octave = atoi(&note[2]);
-        //printf("%c\n",note[1]);
         char accident = note[1];
-        //printf("%c\n", accident);
+
         if (accident=='#')
         {
-            //printf("found a sharp");
+            # SHARP accident
             n += 1;
         }
         else if (accident == 'b')
         {
-            //printf("found a flat");
+            # FLAT accident
             n -= 1;
         }
     }
@@ -82,13 +68,13 @@ int frequency(string note)
     }else if (letter=='G'){
         n -= 2;
     }
-    //printf("%i\n",n);
+
     n += (octave-4) * 12;
 
     float power = n/12.;
-    //printf("%f\n",power);
+
     float f = round(pow(2, power)*440);
-    //printf("%f\n",f);
+
     return f;
 }
 
@@ -99,9 +85,7 @@ bool is_rest(string s)
     {
         return 1;
     }
-
     else
-
     {
         return 0;
     }
---
+++ python/P4ML/w1.py
@@ -0,0 +1,30 @@
+import numpy as np
+import mathplotlib.pyplot as plt
+from time import clock
+
+st = clock()
+
+A = np.ones(n,n)
+B = np.ones(n,n)
+C = np.zeros(n,n)
+
+
+for i in range(n):
+    for j in range():
+        for k in range():
+            C[i, j] = A[j, k] + B[i, k] 
+
+
+x = [1,2,3]
+#plt.plot(X)
+y = [3,4,5]
+
+print "aqui para mi:" , x+y
+
+end = clock()
+
+print end - st
+
+
+
+   
