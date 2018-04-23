bf266d9cf5968b5f487119ab1b043b4d18287e3d
tme <stanermetin@gmail.com>
2018-04-23 16:16:35 +0200

    plot

--- python/P4ML/w1.py
+++ python/P4ML/w1.py
@@ -1,29 +1,31 @@
-import numpy as np
-import mathplotlib.pyplot as plt
-from time import clock
+import numpy as numpy
+import time 
+import matplotlib
+import matplotlib.pyplot as plt 
 
-st = clock()
-
-A = np.ones(n,n)
-B = np.ones(n,n)
-C = np.zeros(n,n)
-
-
-for i in range(n):
-    for j in range():
-        for k in range():
-            C[i, j] = A[j, k] + B[i, k] 
-
-
+t1 = time.clock()
 x = [1,2,3]
-#plt.plot(X)
 y = [3,4,5]
+a = numpy.ones((5,5))
+b = numpy.zeros((5,5))
+print a, b
+print "x+y:", x+y
+print "kidz songz"
+t2 = time.clock()
 
-print "aqui para mi:" , x+y
+x = numpy.arange(0, 10.001, 0.25)
+y = numpy.sin(x)
+z = numpy.cos(x)
+plt.plot(x, y, 'o-', color='red', label='f = sin')
+plt.plot(x, z, 's-', color='blue', label='f = cos')
+plt.legend()
+plt.xlabel('x')
+plt.ylabel('f(x)')
+plt.grid(True)
+plt.show()
 
-end = clock()
 
-print end - st
+
 
 
 
