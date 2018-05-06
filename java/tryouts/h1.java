7701e73a860c3c582a798f35eb88b6a13e971d8c
tme <stanermetin@gmail.com>
2018-05-06 19:33:59 +0200

    jot

---
+++ java/tryouts/h1.java
@@ -0,0 +1,18 @@
+// a try out Java
+public class h1 {
+    
+    public static void main(String[] args) {
+    
+        System.out.println("Hello World");
+	int a=2;
+	int maxInt=2147483647;
+	long muchMore=2147483647 * 10000000;
+	double fraction=99.275;
+	char someChar = 'x';
+
+	String someText="walking in the rain ";
+	System.out.println("loreum ihua " + a + maxInt +muchMore + fraction );
+	System.out.println(someText + "     " +someChar);
+
+    }
+}
