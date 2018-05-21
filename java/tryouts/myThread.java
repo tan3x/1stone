aca8794c7841a60a8843bb99016cd71861fa1539
tme <stanermetin@gmail.com>
2018-05-21 22:50:24 +0200

    faulty threads

--- java/tryouts/myThread.java
+++ java/tryouts/myThread.java
@@ -1,6 +1,24 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class myThread implements Runnable{
 	private int end;
 	private String name;
+
+	
+	// List<String> arrList = new ArrayList<String>();
+ //      arrList.add("Osman");
+ //      arrList.add("Ayse");
+ //      arrList.add(0, "Ozan");
+ //      if(arrList.contains("Osman")) {
+ //         System.out.println("Osman bulundu");
+	//     }
+	
+	// Map<String,String> userMap = new HashMap<String, String>();
+ //      userMap.put("email", "ahmet@example.com");
+ //      userMap.put("name", "Ahmet Zan");
+ //      userMap.put("address", "İstanbul 34000");
+ //      userMap.put("mobile", "5322100000");
 
 	public myThread(String name, int end){
 		this.end = end;
--- java/tryouts/threadLesson.java
+++ java/tryouts/threadLesson.java
@@ -3,7 +3,14 @@ public class threadLesson{
 	public static void main(String args[]){
 		Thread thread1 = new Thread(new myThread("thread1", 6));
 		Thread thread2 = new Thread(new myThread("thread2", 5), "thread2");
+		Thread thread3 = new Thread(new myThread("thread3", 4), "thread3");
+		
+		// thread3.setPriority(Thread.MAX_PRIORITY);
+		// thread1.setPriority(Thread.NORM_PRIORITY);
+		// thread2.setPriority(Thread.MIN_PRIORITY);
+
 		thread1.start();
-		thread2.start();		
+		thread2.start();
+		thread3.start();	
 	}
 }
