eb8b1d6973d8b1065785b7068172ef5a7c71545b
tme <stanermetin@gmail.com>
2018-05-23 08:48:38 +0200

    mundo

--- android/myDemo/app/src/main/java/com/example/tanermetin/mydemo/MainActivity.java
+++ android/myDemo/app/src/main/java/com/example/tanermetin/mydemo/MainActivity.java
@@ -2,16 +2,26 @@ package com.example.tanermetin.mydemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+
     public void buttonMeLlamoClick(View view){
         EditText nombreEditText = (EditText) findViewById(R.id.nombreEditText);
+        Log.i("test","button clicked");
+
+
+        ImageView image = (ImageView) findViewById(R.id.caballoID);
+        image.setImageResource(R.drawable.munnndo);
 
         Toast.makeText(MainActivity.this, "Saludo "+ nombreEditText.getText().toString(), Toast.LENGTH_SHORT).show();
+
+
     }
 
     @Override
