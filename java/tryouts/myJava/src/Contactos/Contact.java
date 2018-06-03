67ed680e344d0b83ba84a0ebec5cd3960d22cd1c
tme <stanermetin@gmail.com>
2018-06-03 12:02:13 +0200

    ルート-Ruto-

--- java/tryouts/myJava/src/Contactos/Contact.java
+++ java/tryouts/myJava/src/Contactos/Contact.java
@@ -4,7 +4,7 @@ public class Contact {
 
     public String name;
     public String email;
-    public String language;
+    private String language;
     public String phone;
     public String genero;
 
--- java/tryouts/myJava/src/com/Mundo/Main.java
+++ java/tryouts/myJava/src/com/Mundo/Main.java
@@ -4,6 +4,7 @@ import Colores.myRojo;
 import Colores.myAzul;
 import Contactos.Contact;
 import Contactos.ContactManager;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
@@ -15,6 +16,7 @@ public class Main {
         myClass.main();
 
         ContactManager objContactManager = new ContactManager();
+        Scanner myScanner = new Scanner(System.in);
 
         Contact elTio = new Contact() ;
         Contact laTia = new Contact() ;
@@ -30,11 +32,23 @@ public class Main {
         objContactManager.addContact(elTio);
         objContactManager.addContact(laTia);
 
-        System.out.println("\nName:"+elTio.name+"\nPhone:"+elTio.phone+"\nLanguage:"+elTio.language+"\nMail:"+elTio.email);
-        System.out.println("\nName:"+laTia.name+"\nPhone:"+laTia.phone+"\nLanguage:"+laTia.language+"\nMail:"+laTia.email);
+        System.out.println("\nName:"+elTio.name+"\nPhone:"+elTio.phone+"\nLanguage: PRIVATE"+"\nMail:"+elTio.email);
+        System.out.println("\nName:"+laTia.name+"\nPhone:"+laTia.phone+"\nLanguage:PRIVATE"+"\nMail:"+laTia.email);
 
         Contact repuesto = objContactManager.searchContact("miTio");
         Contact repuesta = objContactManager.searchContact("miTia");
+
+
+        System.out.println("Quando tiempo bibes en la ciudad?\n");
+        String duration = myScanner.nextLine();
+        System.out.print("Bibo desde "+duration+" meses. ");
+        System.out.print("Quantos anos tienes?");
+
+        int ano = myScanner.nextInt();
+
+        if(35 > ano && ano > 18){System.out.print("Feliz cumpleanos");}
+        else if(ano < 18){   System.out.print("Aprender ja en verano. Hasta pronto");}
+        else{System.out.println("Butique hostels Around Galata.");        }
 
 
     }
