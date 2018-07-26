b3c57bc046357b334ed01051acd61d2d5d21d5a5
taner <stanermetin@gmail.com>
2018-07-26 07:16:36 +0200

    removed clientUDP, essential commUDP

--- android/commUDP/app/src/main/java/com/example/tanermetin/clientudp/UDPServer.java
+++ android/commUDP/app/src/main/java/com/example/tanermetin/clientudp/UDPServer.java
@@ -1,41 +1,24 @@
-<<<<<<< Updated upstream:android/commUDP/app/src/main/java/com/example/tanermetin/clientudp/UDPServer.java
 package com.example.tanermetin.clientudp;
 
-/**
-=======
-package com.example.tanermetin.clientudp; /**
 
-
->>>>>>> Stashed changes:android/clientUDP/app/src/main/java/com/example/tanermetin/clientudp/UDPServer.java
- * Created by tanermetin on 19.06.18.
- */
 import java.io.*;
 import java.net.*;
 
-<<<<<<< Updated upstream:android/commUDP/app/src/main/java/com/example/tanermetin/clientudp/UDPServer.java
-class UDPServer
-{
-=======
+
 
 class UDPServer{
 
 
->>>>>>> Stashed changes:android/clientUDP/app/src/main/java/com/example/tanermetin/clientudp/UDPServer.java
     public static void main(String args[]) throws Exception
     {
         DatagramSocket serverSocket = new DatagramSocket(9876);
 
-<<<<<<< Updated upstream:android/commUDP/app/src/main/java/com/example/tanermetin/clientudp/UDPServer.java
         byte[] receiveData = new byte[1024];
         byte[] sendData = new byte[1024];
-=======
-        byte[] receiveData = new byte[512];
-        byte[] sendData = new byte[512];
 
         System.out.println("LISTENING AT: " +  serverSocket.getLocalSocketAddress());
 
 
->>>>>>> Stashed changes:android/clientUDP/app/src/main/java/com/example/tanermetin/clientudp/UDPServer.java
         while(true)
         {
 
